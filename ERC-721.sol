6e14d6a1c73fa70eb31d7f3d021f7bb2c9613277
VaithiJ <100890851+VaithiJ@users.noreply.github.com>
2022-09-15 17:13:12 +0530

    Update ERC-721.sol

--- ERC-721.sol
+++ ERC-721.sol
@@ -1,4 +1,4 @@
-/*//SPDX-License-Identifier:MIT
+//SPDX-License-Identifier:MIT
 pragma solidity ^0.8.7;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
